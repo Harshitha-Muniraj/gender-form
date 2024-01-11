b07931c43b10e50a876463c119760698f3f4ac5a
Harshitha M <harshithamuniraj22@gmail.com>
2024-01-11 16:22:46 +0530

    gender form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
 
 function App() {
-  const fruits=["Apple","Mango","Litchy","Orange","Avacado","Banana","Mulbery"]
-  const [search,setSearch]=useState("");
-  const [filterSearch,setFilterSearch]=useState(fruits);
-  useEffect(()=>{
-      setFilterSearch(fruits.filter((item)=>{
-      return item.toLowerCase().includes(search.toLowerCase())
-    }))
-      
-    
-  },[search])
-  
+  const [outfit,setOutfit]=useState("Shirt");
+  const [selectedOption,setSelectedOption]=useState(true)
   return (
-    <main>
-      <h3>Search Item</h3>
-      <input type='text' onChange={(e)=>setSearch(e.target.value)} />
-      <ul>
-     {
-      filterSearch.map((item,index)=>{
-        return <li key={index}>{item}</li>
-      })
-     }
-      </ul>
-    </main>
+    <form>
+      <div>
+        <h3>Select Your Gender :</h3>
+        
+        <input type='radio' name='gender' value='Shirt' id='men' checked={selectedOption==true}  onChange={(e)=>{setOutfit(e.target.value);setSelectedOption(true)}}/>
+        <label htmlFor='men'>Men</label>
+       
+        <input type='radio' name='gender' id='women' value='Dress' onChange={(e)=>{setOutfit(e.target.value);setSelectedOption(false)}} />
+        <label htmlFor='women'>women</label>
+      </div>
+      <div>
+        <h3>Select Your {outfit} size</h3>
+        
+          {
+            selectedOption ? 
+            <select name='shirt' id='men'>
+            <option >Select size</option>
+            <option value='small'>Small</option>
+            <option value='medium'>Medium</option>
+            <option value='large'>Large</option>
+            </select>
+            :
+            <select name='dress' id='women'>
+            <option >Select size</option>
+            <option value='2'>2</option>
+            <option value='4'>4</option>
+            <option value='6'>6</option>
+            </select>
+          }
+        
+      </div>
+    </form>
   );
 }
 
